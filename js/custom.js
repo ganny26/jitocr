6aac25499d161a5d9fc3ec8e4b4ed1b4d243d7d0
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-07-10 20:08:37 +0530

    custom js modified

--- js/custom.js
+++ js/custom.js
@@ -1,44 +1,56 @@
 var vertexDetails = [];
 
-			$(document).ready(function () {
-				console.log('loaded');
-				$('img#example').selectAreas({
-					minSize: [10, 10],
-					width: 500,
-					areas: [
-						{
-							x: 10,
-							y: 20,
-							width: 60,
-							height: 100,
-						}
-					]
-				});
-				$('#btnView').click(function () {
-					var areas = $('img#example').selectAreas('areas');
-					displayAreas(areas);
-				});
-			
-				
-			});
-
-		
-
-			function areaToString (area) {
-				var areaObject = {
-					"x":area.x,
-					"y":area.y,
-					"width":area.width,
-					"height":area.height
-				}
-				return areaObject;
+$(document).ready(function () {
+	console.log('loaded');
+	$('img#example').selectAreas({
+		minSize: [10, 10],
+		width: 500,
+		areas: [
+			{
+				x: 10,
+				y: 20,
+				width: 60,
+				height: 100,
 			}
+		]
+	});
+	$('#btnView').click(function () {
+		var areas = $('img#example').selectAreas('areas');
+		displayAreas(areas);
+	});
 
-		
-			// Display areas coordinates in a div
-			function displayAreas (areas) {
-				$.each(areas, function (id, area) {
-					vertexDetails.push(areaToString(area));
-				});
-				console.log(vertexDetails);
-			};
+
+});
+
+
+
+function areaToString(area) {
+	var areaObject = {
+		"x": area.x,
+		"y": area.y,
+		"width": area.width,
+		"height": area.height
+	}
+	return areaObject;
+}
+
+
+// Display areas coordinates in a div
+function displayAreas(areas) {
+	$.each(areas, function (id, area) {
+		vertexDetails.push(areaToString(area));
+	});
+
+	$.ajax({
+		url:"fetchData",
+		type:"GET",
+		data:{"vertex":vertexDetails},
+		success:function(data){
+
+		},
+		error:function(err){
+			
+		}
+	})
+	console.log(vertexDetails);
+};
